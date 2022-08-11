07f7d86c9a7511946ab477ca048c39e088b88ba7
Labaile <hello@labaile.com>
2022-08-11 11:47:14 -0400

    initial setup

---
+++ src/main/java/com/example/booklistapp/BookListAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.booklistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookListAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookListAppApplication.class, args);
+    }
+
+}
